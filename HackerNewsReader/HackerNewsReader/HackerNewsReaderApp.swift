ae2990cc0bcc6ff2b144ec0dafd6b31493bd8e60
Michael Testut <michael.testut@yahoo.com>
2023-01-30 16:08:24 -0800

    xcode project added

---
+++ HackerNewsReader/HackerNewsReader/HackerNewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsReaderApp.swift
+//  HackerNewsReader
+//
+//  Created by Michael Testut on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
