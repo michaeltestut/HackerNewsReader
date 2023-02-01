974aea45a61bd9ee407039fe29f2f5b643e25c44
Michael Testut <michael.testut@yahoo.com>
2023-01-31 22:59:20 -0800

    webviews added

--- HackerNewsReader/HackerNewsReader/Views/DetailView.swift
+++ HackerNewsReader/HackerNewsReader/Views/DetailView.swift
@@ -6,18 +6,36 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct DetailView: View {
     
     let url:String?
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WebView(urlString: url)
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView(url:"http://google.com")
+    }
+}
+
+struct WebView:UIViewRepresentable{
+    let urlString:String?
+    
+    func makeUIView(context: Context) -> WebView.UIViewType {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString=urlString{
+            if let url=URL(string: safeString){
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
     }
 }
