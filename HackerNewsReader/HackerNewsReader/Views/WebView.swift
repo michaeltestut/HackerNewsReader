1cb3018a767dee2529c2df93fe09233014e3da70
Michael Testut <michael.testut@yahoo.com>
2023-02-04 16:53:51 -0800

    Moved WebView Struct into own file

---
+++ HackerNewsReader/HackerNewsReader/Views/WebView.swift
@@ -0,0 +1,27 @@
+//
+//  WebView.swift
+//  HackerNewsReader
+//
+//  Created by Michael Testut on 2/4/23.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView:UIViewRepresentable{
+    let urlString:String?
+    
+    func makeUIView(context: Context) -> WebView.UIViewType {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString=urlString{
+            if let url=URL(string: safeString){
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
